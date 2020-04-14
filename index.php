6e169c272fafa458c590c37b324ba394d4c5b434
Rhys P. Judd <rhyspjudd.dev@gmail.com>
2020-04-14 18:44:44 +0100

    test ssh

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Style CSS -->
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/style.css' ?>">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>FOO</h1>
+    
+</body>
+</html>
